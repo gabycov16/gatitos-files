bb3b6cd4cd09eb63b475fdeb8103f8ee026fdf1d
Gabs <gaby.cov16@gmail.com>
2019-09-10 21:12:30 -0500

    Updates

---
+++ precache-manifest.0b4752231e798ed8683592284078f904.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f651ca4f229abe6461e2f13a081f466",
+    "url": "/gatitos-files/index.html"
+  },
+  {
+    "revision": "7d6028e45b80179ddc9e",
+    "url": "/gatitos-files/static/css/main.bf8042ae.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/gatitos-files/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "7d6028e45b80179ddc9e",
+    "url": "/gatitos-files/static/js/main.073be3af.chunk.js"
+  },
+  {
+    "revision": "774568a538cbcd840e7f",
+    "url": "/gatitos-files/static/js/runtime~main.c09a1f2f.js"
+  }
+]);
